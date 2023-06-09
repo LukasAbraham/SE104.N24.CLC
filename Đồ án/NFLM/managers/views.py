61fa64246f52056afd4ca11dde154f142cf91ef5
Jun0se7en <93256752+Jun0se7en@users.noreply.github.com>
2023-06-09 22:34:16 +0700

    add managers page

---
+++ Đồ án/NFLM/managers/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+def index(request):
+    username = request.user.username
+    context = {
+        'username': username
+    }
+    return render(request, 'pages/managers.html',context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('/login')
